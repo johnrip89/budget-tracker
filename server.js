e97d0fd07c78ed91e5db1b59bc849ba6c5a12384
johnrip89 <lebronjohn89@gmail.com>
2021-11-19 18:14:47 -0700

    redeploying

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const compression = require("compression");
 
 const PORT = process.env.PORT || 3001;
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/pizza-hunt', {
-  useFindAndModify: false,
+  // useFindAndModify: false,
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
