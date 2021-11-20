088e06849bbac8724600cad28d54031f4297a170
johnrip89 <lebronjohn89@gmail.com>
2021-11-19 19:35:31 -0700

    fixed bugs

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,6 +1,4 @@
-const APP_PREFIX = 'BudgetTracker-';     
-const VERSION = 'version_01';
-const CACHE_NAME = APP_PREFIX + VERSION
+const CACHE_NAME = budget-tracker
 
 const FILES_TO_CACHE = [
     '/index.html',
